7a0bf9608cbe66fb21d543ed0c92bcb07425a302
huhuta <smarthorse6@gmail.com>
2019-08-10 19:58:18 +0900

    Fix error when counter not present

--- src/counters/index.js
+++ src/counters/index.js
@@ -2,9 +2,17 @@ import queryString from 'query-string'
 import pullUpCounter from './pullUpCounter'
 import turtleNeckCounter from './turtleNeckCounter'
 
-const { counters } = queryString.parse(window.location.search)
-const allCounter = { pullUpCounter, turtleNeckCounter }
+function getCounter() {
+  const parsed = queryString.parse(window.location.search)
+  const allCounter = { pullUpCounter, turtleNeckCounter }
 
-export default counters.split(',').map((counter) => {
-  return new allCounter[`${counter}Counter`]()
-})
+  if (!parsed.counter) {
+    return allCounter
+  }
+
+  return parsed.counter.split(',').map((counter) => {
+    return new allCounter[`${counter}Counter`]()
+  })
+}
+
+export default getCounter()
