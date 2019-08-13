adf5a73eb732d12111cb9f9714dca7365288306b
huhuta <smarthorse6@gmail.com>
2019-08-14 08:12:40 +0900

    Add chromium open script

---
+++ scripts/open-chromium.sh
@@ -0,0 +1,6 @@
+/Applications/Chromium.app/Contents/MacOS/Chromium \
+ --hide-scrollbars \
+ --remote-debugging-port=9222 \
+ --disable-web-security \
+ --user-data-dir=/tmp/chrome \
+ https://pose.dudaji.org/\?camUrl\=$CAM_URL\&slackUrl=$SLACK_URL
