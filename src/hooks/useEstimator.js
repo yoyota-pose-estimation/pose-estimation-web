dc529b308c8dc376efe8eba414c6a0dc999f7343
yoyota <smarthorse6@gmail.com>
2019-12-01 21:00:29 +0900

    Add useInterval hook

--- src/hooks/useEstimator.js
+++ src/hooks/useEstimator.js
@@ -1,9 +1,10 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import to from 'await-to-js'
 import useCanvas from './useCanvas'
 import getCounter from '../counters'
 import useCheckPose from './useCheckPose'
 import useDrawCanvas from './useDrawCanvas'
+import useInterval from './useInterval'
 
 function getCtx(canvas) {
   const ctx = canvas.getContext('2d')
@@ -29,18 +30,13 @@ export default function({ net, imageElement, canvasRef, intervalDelay }) {
   const [poses, setPoses] = useState([{ keypoints: [] }])
   const [distance, setDistance] = useState(0)
   const canvas = useCanvas({ canvasRef, imageElement })
-  const ctx = getCtx(canvas)
   const counters = getCounter({ canvas, setDistance })
+  const ctx = getCtx(canvas)
 
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      estimatePose({ net, imageElement, setPoses })
-    }, intervalDelay)
-    return () => {
-      clearInterval(intervalId)
-    }
-  }, [net, setPoses, imageElement, intervalDelay])
-
+  useInterval({
+    intervalDelay,
+    fn: () => estimatePose({ net, setPoses, imageElement })
+  })
   useCheckPose({ ctx, poses, counters, imageElement })
   useDrawCanvas({ ctx, poses, counters, distance, imageElement })
 }
